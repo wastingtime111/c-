6b464b974dec63a11d8deff75bce378c2de698ff
wastingtime111 <wastingtime.111900@gmail.com>
2025-02-18 12:04:05 +0600

    Update All_element.cpp

--- All_element.cpp
+++ All_element.cpp
@@ -7,3 +7,21 @@
 #include<algorithm>
 #include<string>
 using namespace std;
+
+//for factorial
+long long int fact(long long int a){
+    if(a==1){return 1;}
+    else{
+        return a*fact(a-1);
+    }
+}
+
+//when you have to know the combination possible of j num of element and each combination you will take y num of element.Here the range of y is from a number(input y) to j.
+long long int numofcom(long long int j, long long int y ){
+if(j==y){
+    return 1;
+}
+else{
+    return (j-y+1)+numofcom(j,y+1);
+}
+}
